f038958e566fcb69c3d2b0bb4cafdd52ee948e19
GNUfault <bluMATRIKZ@gmail.com>
2026-03-03 20:25:23 -0800

    ITS FIXED

--- include/cinux/paging_asm.h
+++
@@ -1,6 +0,0 @@
-#ifndef PAGING_ASM_H
-#define PAGING_ASM_H
-
-void enable_paging(void);
-
-#endif // PAGING_ASM_H
--- kernel/main.c
+++ kernel/main.c
@@ -1,22 +1,18 @@
 #include <cinux/idt.h>
 #include <cinux/pit.h>
 #include <cinux/uptime.h>
-#include <cinux/paging.h>
 #include <cinux/terminal.h>
 #include <cinux/framebuffer.h>
 #include <cinux/font.h>
 #include <cinux/asm.h>
 
 void kmain(framebuffer_t *fb_info) {
-
     idt_init();
     pit_init();
     sti();
     tsc_init();
-    paging_init();
     terminal_init();
     printk("cinux booted\n");
-
 
     for(;;) hlt();
 }
